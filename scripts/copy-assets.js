0af6884cc94fcd67d9d98383d5918abc2c391ed5
BMPMS <milesbryony@googlemail.com>
2026-07-02 15:39:29 +0100

    fix: some build changes

--- scripts/copy-assets.js
+++ scripts/copy-assets.js
@@ -1,10 +1,18 @@
 const fs = require('fs-extra');
+const path = require('path');
 
-// Copy assets to 'dist/assets'
-fs.copySync('./assets', './docs/assets');
+// List of file/folder names to exclude
+const exemptNames = ['nodes.json', 'edges.json'];
+
+const filter = (src) => {
+  const basename = path.basename(src);
+  return !exemptNames.includes(basename);
+};
+
+// Copy assets to 'docs/assets'
+fs.copySync('./assets', './docs/assets', { filter });
 
 // Additional copy
-fs.copySync('./assets', './docs/duForce/assets');
-
+fs.copySync('./assets', './docs/duForce/assets', { filter });
 
 console.log('Assets copied successfully');
